d1030419d9cc655a0861f382ccdbf6044f58742f
Van Co <van@nutanix.com>
2016-05-10 23:38:42 -0700

    Simple App Bar

--- app/client/main.js
+++ app/client/main.js
@@ -1,14 +1,29 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { render } from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import './main.css';
 
 class HelloWorld extends React.Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <AppBar
+          title="Title"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+      </MuiThemeProvider>
     );
   }
 }
 Meteor.startup(() => {
+  // Needed for onTouchTap
+  // Check this repo:
+  // https://github.com/zilverline/react-tap-event-plugin
+  injectTapEventPlugin();
+
   render(<HelloWorld/>, document.getElementById('app'));
 });
