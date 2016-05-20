dc81eceb5f3e9010fc729c47527ea24b0b16ea70
Van Co <van@nutanix.com>
2016-05-19 20:48:19 -0700

    created combonator

---
+++ combonator/imports/client/layout/MainAppBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from 'material-ui/lib/app-bar';
+
+const MainAppBar = () => (
+  <AppBar
+    title="Title"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
+);
+
+export default MainAppBar();
